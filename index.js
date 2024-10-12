79a0319771c05404c177be6c76b94363049281f3
j33s3 <Jessepe04@gmail.com>
2024-10-12 15:22:59 -0700

    Initial commit of portfolio

---
+++ index.js
@@ -0,0 +1,60 @@
+/* -----------------------------------------
+  Have focus outline only for keyboard users 
+ ---------------------------------------- */
+
+const handleFirstTab = (e) => {
+  if(e.key === 'Tab') {
+    document.body.classList.add('user-is-tabbing')
+
+    window.removeEventListener('keydown', handleFirstTab)
+    window.addEventListener('mousedown', handleMouseDownOnce)
+  }
+
+}
+
+const handleMouseDownOnce = () => {
+  document.body.classList.remove('user-is-tabbing')
+
+  window.removeEventListener('mousedown', handleMouseDownOnce)
+  window.addEventListener('keydown', handleFirstTab)
+}
+
+window.addEventListener('keydown', handleFirstTab)
+
+const backToTopButton = document.querySelector(".back-to-top");
+let isBackToTopRendered = false;
+
+let alterStyles = (isBackToTopRendered) => {
+  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+  backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
+  backToTopButton.style.transform = isBackToTopRendered
+    ? "scale(1)"
+    : "scale(0)";
+};
+
+const moreButtons = document.querySelectorAll('.more-btn');
+
+moreButtons.forEach(button => {
+  button.addEventListener('click', (e) => {
+    const project = e.target.closest('.project');
+    const fullDescription = project.querySelector('.full-description');
+
+    if(fullDescription.style.display === 'none' || fullDescription.style.display === ''){
+      fullDescription.style.display = 'block';
+      e.target.textContent = 'Less';
+    } else {
+      fullDescription.style.display = 'none';
+      e.target.textContent = 'More';
+    }
+  });
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 700) {
+    isBackToTopRendered = true;
+    alterStyles(isBackToTopRendered);
+  } else {
+    isBackToTopRendered = false;
+    alterStyles(isBackToTopRendered);
+  }
+});
