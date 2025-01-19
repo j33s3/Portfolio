a141bce1588d79b5e97272e7f3cd2585bf49f657
j33s3 <Jessepe04@gmail.com>
2025-01-19 09:30:29 -0800

    fixed work pages where the body would scroll due to y direction overflow.

--- src/app/work/work-personal/work-personal.component.ts
+++ src/app/work/work-personal/work-personal.component.ts
@@ -41,54 +41,6 @@ export class WorkPersonalComponent implements OnInit {
     })
     
   }
-  // ngOnInit(): void {
-  //   // Select the vertical line element
-  //   const verticalLine = document.querySelector('.vertical-container') as HTMLElement;
 
-  //   // Listen to the scroll event
-  //   window.addEventListener('scroll', () => {
-  //     const scrollPosition = window.scrollY + window.innerHeight;
-  //     console.log(scrollPosition);
 
-  //     // Update the height of the vertical line
-  //     if (verticalLine) {
-  //       verticalLine.style.height = `${scrollPosition}px`;
-  //     }
-  //   });
-  // }
 }
-
-
-
-
-
-// export class WorkPersonalComponent implements AfterViewInit{
-
-//   constructor() {}
-
-//   ngAfterViewInit(): void {
-//     const circles = document.querySelectorAll('.circle-break');
-//     const verticalLine = document.querySelector('.vertical-line') as HTMLElement;
-  
-//     window.addEventListener('scroll', () => {
-//       const scrollPosition = window.scrollY + window.innerHeight;
-  
-//       if(verticalLine) {
-//         verticalLine.style.height = `${scrollPosition}px`
-//       }
-  
-//       circles.forEach((circle: Element) => {
-//         const circlePosition= circle.getBoundingClientRect().top + window.scrollY + circle.clientHeight / 2;
-  
-//         if(scrollPosition > circlePosition - 10 && scrollPosition < circlePosition + 10) {
-//           circle.classList.add('filled');
-//         } else {
-//           circle.classList.remove('filled');
-//         }
-  
-//       })
-//     })
-//   }
-
-
-// }
