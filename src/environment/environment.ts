f2069faefeb9ea887a3cfc85578b4cae113874dd
Jesse Peterson <Jessepe04@gmail.com>
2025-03-29 22:01:50 -0700

    Added slight change to default in environment

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
     // baseUrl: 'prod/',
     // STSUrl: 'https://c4to99eiba.execute-api.us-west-2.amazonaws.com/prod/get-sts-token'
     baseUrl: "prod/",
-    STSUrl: process.env['AWS_API_STS_URL'] || "http://localhost: 3000"
+    STSUrl: process.env['AWS_API_STS_URL'] || "http://localhost:3000"
 }
