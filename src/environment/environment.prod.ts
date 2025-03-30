0a6ac99c80127a4680ab64e8bbc1ead196b05001
Jesse Peterson <Jessepe04@gmail.com>
2025-03-29 22:54:30 -0700

    Debugging environment variables

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -3,6 +3,7 @@ console.log("STS URL: ", process.env['AWS_API_STS_URL']);
 
 export const environment = {
     production: true,
-    baseUrl: process.env['AWS_API_BASE_URL'] || "http://localhost:3000",
+    baseUrl: '/prod/',
+    // baseUrl: process.env['AWS_API_BASE_URL'] || "http://localhost:3000",
     STSUrl: process.env['AWS_API_STS_URL'] || "http://localhost:3000"
 }
